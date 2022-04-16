0a01ee1a8dc52c4e48ee5f86b43dd12f05e72e8d
Grant Belcher <gtbelch@gmail.com>
2022-04-16 14:52:21 -0700

    begins formatting right section of buttons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Test from "./components/Test/Test";
 import Trig from "./components/Trig/Trig";
 import GridCenter from "./components/GridCenter";
+import GridRight from "./components/GridRight";
 
 const App = () => (
   <div className="container">
     <div className="bottom-section">
       <Trig />
       <GridCenter />
+      <GridRight />
     </div>
   </div>
 );
---
+++ client/src/components/GridRight.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function GridRight() {
+  return (
+    <div className="grid-right">
+      <div className="button"></div>
+      <div className="button"></div>
+      <div className="button"></div>
+      <div className="button"></div>
+      <div className="button"></div>
+      <div className="button"></div>
+    </div>
+  );
+}
+
+export default GridRight;
