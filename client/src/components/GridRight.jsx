69781602ab67ecf23c1c5c985aafb7abe26ee58c
Grant Belcher <gtbelch@gmail.com>
2022-04-17 20:47:21 -0700

    begins styling mobile

--- client/src/components/GridRight.jsx
+++ client/src/components/GridRight.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 function GridRight() {
   return (
-    <div className="grid-right">
-      <div className="button"></div>
-      <div className="button"></div>
-      <div className="button"></div>
-      <div className="button"></div>
+    <div className="buttons-right">
+      <div className="right-row-1">
+        <div className="button"></div>
+        <div className="button"></div>
+      </div>
+      <div className="right-row-2">
+        <div className="button"></div>
+        <div className="button"></div>
+      </div>
       <div className="button"></div>
       <div className="button"></div>
     </div>
