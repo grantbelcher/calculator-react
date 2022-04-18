517481ff5b42aca948b1f022848166c04fc4bb2c
Grant Belcher <gtbelch@gmail.com>
2022-04-18 16:45:16 -0700

    creates right grid of buttons

--- client/src/components/GridRight.jsx
+++ client/src/components/GridRight.jsx
@@ -3,16 +3,20 @@ import React from "react";
 function GridRight() {
   return (
     <div className="buttons-right">
-      <div className="right-row-1">
-        <div className="button"></div>
-        <div className="button"></div>
+      <div className="right-row top">
+        <div>1</div>
+        <div>2</div>
       </div>
-      <div className="right-row-2">
-        <div className="button"></div>
-        <div className="button"></div>
+      <div className="right-row top">
+        <div>3</div>
+        <div>4</div>
       </div>
-      <div className="button"></div>
-      <div className="button"></div>
+      <div className="right-row bottom">
+        <div>5</div>
+      </div>
+      <div className="right-row bottom">
+        <div>6</div>
+      </div>
     </div>
   );
 }
