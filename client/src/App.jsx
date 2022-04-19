3a547edaa82abaaf5faf5998a227fe59cd3d6147
Grant Belcher <gtbelch@gmail.com>
2022-04-19 00:46:15 -0700

    adds placeholder for options component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,11 @@ import Test from "./components/Test/Test";
 import Trig from "./components/Trig/Trig";
 import GridCenter from "./components/GridCenter";
 import GridRight from "./components/GridRight";
+import Options from "./components/Options";
 
 const App = () => (
   <div className="container">
+    <Options />
     <div className="bottom-section">
       <Trig />
       <GridCenter />
---
+++ client/src/components/Options.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Options() {
+  return <div className="options">options</div>;
+}
+
+export default Options;
