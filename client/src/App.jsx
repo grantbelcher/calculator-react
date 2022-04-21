520cf1256f487757324311f961bb31650c38e080
Grant Belcher <gtbelch@gmail.com>
2022-04-21 16:54:41 -0700

    moves calc buttons to bottom of container when no equations are present

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Test from "./components/Test/Test";
 import Trig from "./components/Trig/Trig";
 import GridCenter from "./components/GridCenter";
@@ -6,18 +6,21 @@ import GridRight from "./components/GridRight";
 import Options from "./components/Options";
 import Equations from "./components/Equations";
 
-const App = () => (
-  <div className="container">
-    <Equations />
-    <div className="calculator-api">
-      <Options />
-      <div className="bottom-section">
-        <Trig />
-        <GridCenter />
-        <GridRight />
+const App = () => {
+  const [prevEquations, setPrevEquations] = useState([]);
+  return (
+    <div className="container">
+      <Equations prevEquations={prevEquations} />
+      <div className="calculator-api">
+        <Options />
+        <div className="bottom-section">
+          <Trig />
+          <GridCenter />
+          <GridRight />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
--- client/src/components/Equations.jsx
+++ client/src/components/Equations.jsx
@@ -2,32 +2,32 @@ import React, { useState } from "react";
 import Equation from "./Equation";
 import { useTransition } from "react";
 
-let equation = [
-  "4+5=9",
-  "5*6=30",
-  "9/3=3",
-  "21/3=7",
-  "32/4=8",
-  "32/4=8",
-  "32/4=8",
-  "32/4=8",
-  "32/4=8",
-  "1000/10/25/4=1",
-];
+// let equation = [
+//   "4+5=9",
+//   "5*6=30",
+//   "9/3=3",
+//   "21/3=7",
+//   "32/4=8",
+//   "32/4=8",
+//   "32/4=8",
+//   "32/4=8",
+//   "32/4=8",
+//   "1000/10/25/4=1",
+// ];
 
-function Equations() {
+function Equations({ prevEquations }) {
   const [focus, setFocus] = useState(0);
 
   const clickHandler = (indexOfEquation) => {
     setFocus(indexOfEquation);
   };
-  const elements = equation.map((equation, i) => {
+  const equationComponents = prevEquations.map((equation, i) => {
     if (i === focus)
       return <Equation focus={true} index={i} clickHandler={clickHandler} />;
     return <Equation focus={false} index={i} clickHandler={clickHandler} />;
   });
 
-  return <div className="equation-container">{elements}</div>;
+  return <div className="equation-container">{equationComponents}</div>;
 }
 
 export default Equations;
