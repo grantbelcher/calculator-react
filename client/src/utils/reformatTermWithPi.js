f4bba5f5be8890dfb5aab2ed79e14531f56c2180
Grant Belcher <gtbelch@gmail.com>
2022-04-24 17:36:39 -0700

    defines function to evaluate equations containing Pi

--- client/src/utils/reformatTermWithPi.js
+++ client/src/utils/reformatTermWithPi.js
@@ -19,28 +19,67 @@ function replacePi(string, indexes) {
   let newString = [];
   // iterate over the string
   for (i = 0; i < string.length; i++) {
+    // if a character other than Pi is stored at the current index of the string
     if (!indexes.includes(i)) {
+      // add the non-pi character to the array that we will join
       newString.push(string[i]);
+      // otherwise, we know that the char at the index is Pi
     } else {
+      // instead of adding the character pi, we will add the javascript representation that the eval function can read
+      // we will also surround it with multiplication signs to automatically multiply it with the surounding terms
       newString.push("*Math.PI*");
     }
   }
+  // return the array of all elements of the term, that we will join together into the appropriate format
   return newString;
 }
-
+// call this function  with a nasty term with many Pis multiplied by many factors
 function reformatTermsWithPi(string) {
+  // find all indices of Pi in nasty string
   const allIndexes = findAllIndexOfPi(string);
-  const result = replacePi(myTest, allIndexes);
+  // replace Pi with Math.Pi in a format that JS eval function can read
+  // WAIT WAIT WAIT, If something breaks check here because i replaced myTest with string
+  const result = replacePi(string, allIndexes);
+  // join all the elements of array stored in result
   const joinedString = result.join("");
+  // split the string based on duplicated multiplication signs that will result from calling replacePi
   const removeDuplicates = joinedString.split("**");
+  // rejoin the array without ** duplicates with only a single *
   let rejoin = removeDuplicates.join("*");
 
+  // trim * off the front of term which will occur if pi is the first character
   if (rejoin[0] === "*") {
     rejoin = rejoin.slice(1);
   }
-
+  // trim * off the back of term which will occur if pi is the last character
   if (rejoin[rejoin.length - 1] === "*") {
     rejoin = rejoin.slice(0, rejoin.length - 1);
   }
   return rejoin;
 }
+
+// uncomment and run the final function to test
+// const myTest = 'π23π2443ππ4π23ππ'
+// reformatTermsWithPi(myTest)
+
+let equation = "23π + π + 22 - 2π3 + ππ + 23π2443ππ4π23 + ππ3 + 2ππ + ππ3ππ23";
+
+console.log(reformatTermsWithPi("22"));
+
+function evaluateNastyEquationWithPi(eq) {
+  // split eq based on operators
+  const componentsOfEquation = eq.split(" ");
+  // create array of reformated terms containing pI
+  console.log(componentsOfEquation);
+  const reformattedTerms = componentsOfEquation.map((term) => {
+    if (term === "+" || term === "-" || term === "/" || term === "*") {
+      return term;
+    } else {
+      return reformatTermsWithPi(term);
+    }
+  });
+  const joinAllTerms = reformattedTerms.join("");
+  return joinAllTerms;
+}
+
+evaluateNastyEquationWithPi(equation);
