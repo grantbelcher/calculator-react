3a1792c9715c582c945f92448d9133e8d351b967
Grant Belcher <gtbelch@gmail.com>
2022-04-25 17:43:04 -0700

    creates function to reformat all trig functions that can be read by eval()

---
+++ client/src/utils/reformatTrigFunction.js
@@ -0,0 +1,69 @@
+let string = "sin(30) + tan(40)sin(15)cos(30)sin(32) + cos(21)";
+
+function reformatSin(expression) {
+  return expression.replace(/sin/g, `*Math.sin`);
+}
+function reformatCos(expression) {
+  return expression.replace(/cos/g, `*Math.cos`);
+}
+function reformatTan(expression) {
+  return expression.replace(/tan/g, `*Math.tan`);
+}
+
+function reformatTrigFuncs(string) {
+  let newString = reformatSin(string);
+  newString = reformatCos(newString);
+  newString = reformatTan(newString);
+  return newString;
+}
+// "sin(30) + tan(40)sin(15)cos(30)sin(32) + cos(21)10"
+let reformattedExpression = reformatTrigFuncs(string);
+
+function multiplyAfterParenthesis(string) {
+  let newString = "";
+  for (i = 0; i < string.length; i++) {
+    if (string[i] === ")" && string[i + 1] !== " " && string[i + 1] !== "*") {
+      newString += string[i] + "*";
+    } else {
+      newString += string[i];
+    }
+  }
+  return newString;
+}
+
+let x = multiplyAfterParenthesis(reformattedExpression);
+
+function removeExcessMultiplier(string) {
+  // create a new array to store
+  // split expression into individual elements
+  let arrayOfElements = string.split(" ");
+  console.log(arrayOfElements);
+  // iterate over array of elements
+  for (let i = 0; i < arrayOfElements.length; i++) {
+    // if an element begins with a multiplier, then it is excess and needs to       be removed
+
+    if (
+      arrayOfElements[i][0] === "*" &&
+      arrayOfElements[i][arrayOfElements[i].length - 1] === "*"
+    ) {
+      arrayOfElements[i] = arrayOfElements[i].slice(
+        1,
+        arrayOfElements[i].length - 1
+      );
+    } else if (arrayOfElements[i][0] === "*") {
+      arrayOfElements[i] = arrayOfElements[i].slice(1);
+    } else if (arrayOfElements[i][arrayOfElements[i].length - 1] === "*") {
+      arrayOfElements = arrayOfElements[i].slice(
+        0,
+        arrayOfElements[i].length - 1
+      );
+    } else {
+      arrayOfElements[i] = arrayOfElements[i];
+    }
+  }
+  return arrayOfElements.join("");
+}
+
+let finalTest = removeExcessMultiplier(x);
+console.log(eval(finalTest));
+// console.log(eval(finalTest))
